5603fd206ed4b17614ca5f42d0a462b616c0ee93
Nahia Ganuza <72313513+NahiaGanuza@users.noreply.github.com>
2023-11-23 17:25:07 +0000

    Resolviendo code smell de Game.java

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -8,7 +8,7 @@ public class Game {
 
 	protected Game() { } // prevents instantiation (static class
 
-	public static ColorScheme COLORS = new ColorScheme();
+	public static final ColorScheme COLORS = new ColorScheme();
 	public static final Window WINDOW = new Window("2048");
 	public static final Controls CONTROLS = new Controls();
 	public static final Board BOARD = new Board(4);
