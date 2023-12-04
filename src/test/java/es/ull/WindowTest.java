39bb685c97f174f6ce8559a65c8c43f022ae42b7
Nahia Ganuza <72313513+NahiaGanuza@users.noreply.github.com>
2023-12-04 19:23:01 +0000

    ultimos cambios window test

--- src/test/java/es/ull/WindowTest.java
+++ src/test/java/es/ull/WindowTest.java
@@ -9,21 +9,17 @@ public class WindowTest {
 
     @Test
     public void testConstructor() {
-        if (!GraphicsEnvironment.isHeadless()) {
-            // Arrange
-            String title = "Test";
+        // Arrange
+        String title = "Test";
 
-            // Act
-            Window window = new Window(title);
+        // Act
+        Window window = new Window(title);
 
-            // Assert
-            assertNotNull(window);
-            assertEquals(title, window.getTitle());
-            assertFalse(window.isResizable());
-            assertTrue(window.isFocusable());
-            assertTrue(window.isVisible());
-        } else {
-            System.out.println("Skipping AWT-related test in headless environment.");
-        }
+        // Assert
+        assertNotNull(window);
+        assertEquals(title, window.getTitle());
+        assertFalse(window.isResizable());
+        assertTrue(window.isFocusable());
+        assertTrue(window.isVisible());
     }
 }
