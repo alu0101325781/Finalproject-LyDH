d3a70000c522c0bfe0c6328ed3ae08fc42b16321
FrancescoMarelli <alu0101161730@ull.edu.es>
2023-12-04 19:38:04 +0000

    test main

---
+++ src/test/java/es/ull/MainTest.java
@@ -0,0 +1,12 @@
+package es.ull;
+
+import org.junit.jupiter.api.Test;
+
+public class MainTest {
+    @Test
+    public void testMain() {
+        // Arrange
+        Menu menu = new Menu();
+    }
+
+    }
