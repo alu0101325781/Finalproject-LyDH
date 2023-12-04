ac8135928bead81512a48dbb409dee74fd2b913d
alu0101221953 <72502501+alu0101221953@users.noreply.github.com>
2023-12-04 18:49:16 +0000

    Create LeaderboardTest.java

---
+++ src/test/java/es/ull/LeaderboardTest.java
@@ -0,0 +1,49 @@
+package es.ull;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class LeaderboardTest {
+      
+      @Test
+      public void testConstructor() {
+          // Arrange
+          String playerName = "Charlie";
+          int initialScore = 75;
+  
+          // Act
+          Player player = new Player(playerName, initialScore);
+  
+          // Assert
+          assertNotNull(player);
+          assertEquals(playerName, player.getName());
+          assertEquals(initialScore, player.getScore());
+      }
+  
+      @Test
+      public void testGetName() {
+          // Arrange
+          String playerName = "Alice";
+          Player player = new Player(playerName, 100);
+  
+          // Act
+          String name = player.getName();
+  
+          // Assert
+          assertEquals(playerName, name);
+      }
+  
+      @Test
+      public void testGetScore() {
+          // Arrange
+          int initialScore = 50;
+          Player player = new Player("Bob", initialScore);
+  
+          // Act
+          int score = player.getScore();
+  
+          // Assert
+          assertEquals(initialScore, score);
+      }
+  
+}
