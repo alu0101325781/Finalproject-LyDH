32f8c505dcb742bbe0d9afe9835beb06932bddfb
FrancescoMarelli <alu0101161730@ull.edu.es>
2023-12-04 18:18:10 +0000

    testColor

---
+++ src/test/java/es/ull/ColorSchemeTest.java
@@ -0,0 +1,16 @@
+package es.ull;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.awt.*;
+public class ColorSchemeTest {
+    @Test
+    public void testColorScheme() {
+        if (!GraphicsEnvironment.isHeadless()) {
+            es.ull.ColorScheme colorScheme = new ColorScheme();
+            assertTrue(colorScheme instanceof ColorScheme);
+        } else {
+            System.out.println("Skipping AWT-related test in headless environment.");
+        }
+    }
+}
