e6113dc27ec7efa44e8ab774e6546037bb3a0974
FrancescoMarelli <alu0101161730@ull.edu.es>
2023-12-04 18:33:11 +0000

    testColor update

--- src/test/java/es/ull/ColorSchemeTest.java
+++ src/test/java/es/ull/ColorSchemeTest.java
@@ -1,15 +1,26 @@
 import es.ull.ColorScheme;
+import es.ull.Game;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 import java.awt.*;
 public class ColorSchemeTest {
     @Test
-    public void testColorScheme() {
-        if (!GraphicsEnvironment.isHeadless()) {
-            ColorScheme colorScheme = new ColorScheme();
-            assertTrue(colorScheme instanceof ColorScheme);
-        } else {
-            System.out.println("Skipping AWT-related test in headless environment.");
-        }
+    public void testUpdateColors() {
+        ColorScheme colorScheme = new ColorScheme();
+        Game.isDarkMode = false;
+
+        //colorScheme.updateColors();
+        assertEquals(new Color(238,228,218,90), colorScheme.getTileBackground(0));
+        assertEquals(new Color(0XEEE4DA), colorScheme.getTileBackground(2));
+        assertEquals(new Color(0XEDE0C8), colorScheme.getTileBackground(4));
+        assertEquals(new Color(0XF2B179), colorScheme.getTileBackground(8));
+        assertEquals(new Color(0XF59563), colorScheme.getTileBackground(16));
+        assertEquals(new Color(0XF67C5F), colorScheme.getTileBackground(32));
+        assertEquals(new Color(0XF65E3B), colorScheme.getTileBackground(64));
+        assertEquals(new Color(0XEDCF72), colorScheme.getTileBackground(128));
+        assertEquals(new Color(0XEDCC61), colorScheme.getTileBackground(256));
+        assertEquals(new Color(0XEDC850), colorScheme.getTileBackground(512));
+        assertEquals(new Color(0XEDC53F), colorScheme.getTileBackground(1024));
+        colorScheme.updateColors();
     }
 }
