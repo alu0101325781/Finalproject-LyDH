a0fcaee0ac6b4b75c590c87fa3c5eb0074f255de
FrancescoMarelli <alu0101161730@ull.edu.es>
2023-12-05 09:10:26 +0000

    update mvn tests

--- src/test/java/es/ull/ColorSchemeTest.java
+++ src/test/java/es/ull/ColorSchemeTest.java
@@ -1,6 +1,8 @@
 package es.ull;
 
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import java.awt.*;
 
