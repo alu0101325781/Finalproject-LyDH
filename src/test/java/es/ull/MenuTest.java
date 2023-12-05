dcf6f2c4067f7014137a48b6fbade13e579b87b7
FrancescoMarelli <alu0101161730@ull.edu.es>
2023-12-05 12:59:30 +0000

    add test menu

--- src/test/java/es/ull/MenuTest.java
+++ src/test/java/es/ull/MenuTest.java
@@ -1,13 +1,21 @@
 package es.ull;
 
 import org.junit.jupiter.api.Test;
+
+import java.awt.*;
+
 import static org.junit.jupiter.api.Assertions.*;
 public class MenuTest {
     @Test
     public void testMenu() {
-        Menu menu = new Menu();
-        assertTrue(menu instanceof Menu);
-        assertEquals(menu.getClass().getName(), "es.ull.Menu");
-        assertNotNull(menu);
+        if (!GraphicsEnvironment.isHeadless()) {
+            Menu menu = new Menu();
+            assertTrue(menu instanceof Menu);
+            assertEquals(menu.getClass().getName(), "es.ull.Menu");
+            assertNotNull(menu);
+        } else {
+            System.out.println("Skipping AWT-related test in headless environment.");
+        }
     }
+
 }
