c91abdf5b465ce3a717f9acd1a0fb2e1a5d6910c
FrancescoMarelli <alu0101161730@ull.edu.es>
2023-12-05 13:07:44 +0000

    add test colorscheme

--- src/test/java/es/ull/MenuTest.java
+++ src/test/java/es/ull/MenuTest.java
@@ -13,9 +13,19 @@ public class MenuTest {
             assertTrue(menu instanceof Menu);
             assertEquals(menu.getClass().getName(), "es.ull.Menu");
             assertNotNull(menu);
+            assertFalse(menu.isResizable());
         } else {
             System.out.println("Skipping AWT-related test in headless environment.");
         }
     }
+    @Test
+    public void testStrings() {
+        assertEquals(Menu.NORMAL, "Normal");
+        assertEquals(Menu.DIFICIL, "Difícil");
+        assertEquals(Menu.MUYDIFICIL, "Muy Difícil");
+        assertEquals(Menu.IMPOSIBLE, "Imposible");
+    }
+
+
 
 }
