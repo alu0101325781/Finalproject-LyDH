949492b7706d338bd8894dde9be6f0fc65faee42
FrancescoMarelli <alu0101161730@ull.edu.es>
2023-12-06 17:57:47 +0000

    Update MainTest.java

--- src/test/java/es/ull/MainTest.java
+++ src/test/java/es/ull/MainTest.java
@@ -1,6 +1,9 @@
 package es.ull;
 
 import org.junit.jupiter.api.Test;
+
+import java.awt.*;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class MainTest {
@@ -12,9 +15,13 @@ public class MainTest {
     }
     @Test
     public void testMenuCreation() {
-        Menu menu = new Menu();
-        assertTrue(menu instanceof Menu);
-        assertEquals(menu.getClass().getName(), "es.ull.Menu");
-        assertNotNull(menu);
+        if(!GraphicsEnvironment.isHeadless()) {
+            Menu menu = new Menu();
+            assertTrue(menu instanceof Menu);
+            assertEquals(menu.getClass().getName(), "es.ull.Menu");
+            assertNotNull(menu);
+        } else {
+            System.out.println("Headless environment detected, skipping test");
+        }
     }
 }
